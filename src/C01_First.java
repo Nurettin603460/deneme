3f41d1629e5938d05544c7c025088eca4d081171
Nurettin KAYA <nurettinkaya@gmail.com>
2023-01-13 20:36:08 +0300

    2.gün

--- src/C01_First.java
+++ src/C01_First.java
@@ -2,6 +2,8 @@ public class C01_First {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("merhaba");
+        System.out.println( );
+        System.out.println("deneme");
 
     }
 }
